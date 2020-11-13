664f105b4262710b7d11d951ea41d8df404e63ea
syugoudada <to154514asa134009@gmail.com>
2020-11-13 16:40:06 +0900

    変更

---
+++ Login/index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+print($_SESSION['user']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>成功</h1>
+  <a href="./php/Password_Change/password_change.php"></a>
+</body>
+</html>
