7a3921efe59f210abbc8b4e65d28f080588a0355
syugoudada <to154514asa134009@gmail.com>
2020-11-24 17:34:21 +0900

    css

--- Product_Register/Register.php
+++ Product_Register/Register.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="../Css/register.css">
   <title>Document</title>
 </head>
 <body>
@@ -26,6 +27,7 @@
               $('#subgenre').show();
               click.val("-");
               click.removeClass('plus');
+              $('.register').append('<input type="text" placeholder="こんにちは">');
             }else{
               $('#subgenre').hide();
               click.val('+');
@@ -34,16 +36,18 @@
           });
         });  
       </script>
-      <select name="subgenre" id="subgenre" hidden>
+      <select name="subgenre" id="subgenre" class="subgenre">
       <option disabled selected>ジャンルを選択してください</option>
+      <option>新規追加</option>
       </select>
       <input type="button" id="plusbutton" class="plus" value = "+"><br>
+      <div class="register">
+      </div>
       価格:<input type="text" name="price"><br>
       引用:<input type="text" name="url"><br>
       <input type="file" name="myfile">
       <button name="submit">Upload</button>
     </form>
   </div>
-  
 </body>
 </html>
