523f6399f19b05ae6d9a1d4b3fd3d69ddd72e72f
tingtt <t.t.launcher.real@gmail.com>
2020-12-01 16:11:51 +0900

    Dockerでの実装のためにconfigを変更

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', '127.0.0.1');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'mysql');
 define('DB_NAME', 'intelli_base');
